469ef5eb12e1f0c994d5d485c9928fe29058f9cc
ionut ciornei <Pantelimon194@gmail.com>
2024-01-10 07:36:53 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import '../src/styles/index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Clover__beauty'>
         <App />
     </BrowserRouter>
   </React.StrictMode>
